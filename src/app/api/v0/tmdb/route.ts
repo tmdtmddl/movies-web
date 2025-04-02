6583e4ca5f8f01054b264a3ee9f69f2e95d268d6
tmdtmddl <tmddlqkqh@naver.com>
2025-04-02 17:28:29 +0900

    add

--- src/app/api/v0/tmdb/route.ts
+++ src/app/api/v0/tmdb/route.ts
@@ -1,20 +1,19 @@
-export async function GET() {
-  const url =
-    "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+import { tmdbOptions } from "@/types/tmdb";
 
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
-    },
-  };
-
-  try {
-    const res = await fetch(url, options);
-    const data = await res.json();
-    return Response.json({ success: true, data });
-  } catch (error: any) {
-    return Response.json({ success: false });
+export async function GET(req: Request) {
+  const mid = new URL(req.url).searchParams.get("mid");
+  let url = "";
+  if (!mid) {
+    url =
+      "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
+  } else {
+    url = `https://api.themoviedb.org/3/movie/${mid}?language=en-US`;
   }
+
+  const res = await fetch(url, tmdbOptions());
+  const data = await res.json();
+  if (data.data && !data.data.success) {
+    return Response.json(data.data, { status: 500 });
+  }
+  return Response.json(data, { status: 200 });
 }
