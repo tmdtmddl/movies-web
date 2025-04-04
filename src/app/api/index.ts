334016a46f1e2f7900cbb2110531e27e3d7dbd3c
tmdtmddl <tmddlqkqh@naver.com>
2025-04-04 17:26:12 +0900

    animals scroll

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -19,3 +19,4 @@ export default response;
 
 response.send<{ message: string }>({ message: "message" });
 response.error<{ message: string }>({ message: "message" });
+//axios 쓰는법 (엑시오스랑 fetch중 골라서 쓰기) 엑시오스가 에러핸들링용이
